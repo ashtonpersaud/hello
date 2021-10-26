700c36ecaa108eec15068df8354c736278b09569
ashtonpersaud <92948501+ashtonpersaud@users.noreply.github.com>
2021-10-26 19:58:51 -0400

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,27 @@
+from adapt.intent import IntentBuilder
+from mycroft import MycroftSkill, intent_handler
+import serial
+import time
+import subprocess
+
+class GoKnightsSkill(MycroftSkill):
+
+    def __init__(self):
+        super().__init__()
+        self.learning = True
+
+    def initialize(self):
+        my_setting = self.settings.get('my_setting')
+
+    @intent_handler('greetings.intent')
+    def handle_not_are_you_intent(self, message):
+        self.speak_dialog("Hello fellow knight")
+        ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+        ser.flush()
+        ser.write(b"greeting")      
+
+    def stop(self):
+        pass
+
+def create_skill():
+    return GoKnightsSkill()

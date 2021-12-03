0874a0cdf41054ebebb812c255d30898446b0c88
ashtonpersaud <92948501+ashtonpersaud@users.noreply.github.com>
2021-12-03 09:52:02 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -25,9 +25,9 @@ class helloSkill(MycroftSkill):
         serC = serial.Serial('/dev/ttyACM2', 9600, timeout=1)
         serC.flush()
         serC.write(b"wave")
-        serD = serial.Serial('/dev/ttyACM3', 9600, timeout=1)
-        serD.flush()
-        serD.write(b"wave")
+        #serD = serial.Serial('/dev/ttyACM3', 9600, timeout=1)
+        #serD.flush()
+        #serD.write(b"wave")
         time.sleep(1.5)
         self.speak_dialog("hello knight")
 
